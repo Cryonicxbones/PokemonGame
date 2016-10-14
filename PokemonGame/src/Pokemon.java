1dbc82f7cbe00adc4b64034a2070ab22a849d4d3
Anthony <anthony.tesoriero@stonybrook.edu>
2016-10-13 21:09:53 -0400

    Third change

--- PokemonGame/src/Pokemon.java
+++ PokemonGame/src/Pokemon.java
@@ -1,4 +1,19 @@
 
 public class Pokemon {
-
+	private int hitPoints;
+	private int attack;
+	private int defense;
+	private int spAttack;
+	private int spDefense;
+	private int speed;
+	
+	public Pokemon(){}
+	public Pokemon(int hp, int a, int d, int spAtt, int spDef, int spd){
+		hitPoints = hp;
+		attack = a;
+		defense = d;
+		spAttack = spAtt;
+		spDefense = spDef;
+		speed = spd;
+	}
 }
